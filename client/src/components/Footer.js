e0f7f66452673da14b4eb375e6c6bd17b66a0385
Dustin Schulte <dschulte1982@hotmail.com>
2020-10-08 15:54:21 -0700

    Landing page structure in-place and mostly styled

---
+++ client/src/components/Footer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default function Footer() {
+    return (
+        <>
+        <div id="footer-master-container">
+          <div id="footer-column-1">
+            <div id="my-info">
+                <span className="personal-info">dustin.schulte@gmail.com</span>
+                <span className="personal-info">+509.969.2978</span>
+            </div>
+            <div id="trademark">© 2020 Dust 'Em Off</div>
+          </div>
+          <div id="footer-column-2">
+              <div id="social-media-links">
+                  <div id="linked-in"><a className="footer-links" href="https://linkedin.com/in/dustin-schulte07/" target="_blank">LinkedIn</a></div>
+                  <div id="github"><a className="footer-links" href="https://github.com/Dschulte1982" target="_blank">GitHub</a></div>
+              </div>
+          </div>
+        </div>
+        </>
+    )
+}
---
+++ client/src/components/Search.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Search() {
+
+    return (
+        <>
+          <div id="search-master-container">
+            <input id="search-bar" type="text" placeholder="Explore Collections..."></input>
+          </div>
+        </>
+    )
+}
